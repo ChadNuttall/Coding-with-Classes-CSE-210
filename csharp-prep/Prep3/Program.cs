bd92526febaa9e888b1c1f523128e04972d4d616
ChadNuttall <chadnuttall@byui.edu>
2024-04-26 10:48:20 -0600

    Guess the Magic Number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("What is the magic number?");
+        //string userInput = Console.ReadLine(); 
+        //int magicNumber = int.Parse(userInput);
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("Guess the magic number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("That is the magic number!");
+            }
+        }
     }
 }
