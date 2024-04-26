dae93b2ca8662f40fc9699633cfaa31908fbc657
ChadNuttall <chadnuttall@byui.edu>
2024-04-26 11:39:53 -0600

    Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(favoriteNumber);
+
+        DisplayResults(userName,squareNumber);
+
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            return userName;
+        }
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Please enter your favorite number: ");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+
+            return favoriteNumber;
+        }
+        static int SquareNumber(int favoriteNumber)
+        {
+            int squareNumber = favoriteNumber * favoriteNumber;
+            
+            return squareNumber;
+        }
+        static void DisplayResults(string userName, int squareNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your favorite number is {squareNumber}");
+        }
     }
 }
