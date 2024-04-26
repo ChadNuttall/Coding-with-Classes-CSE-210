bee7edae3deac05552271898ec40f010b74b1757
ChadNuttall <chadnuttall@byui.edu>
2024-04-26 10:26:39 -0600

    Grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your Grade Percentage: ");
+        string userGivenGrade = Console.ReadLine();
+
+        int gradePercenatge = int.Parse(userGivenGrade);
+        string letterGrade = "";
+
+        if (gradePercenatge >= 90){
+            letterGrade = "A";
+        }
+        else if (gradePercenatge >= 80 && gradePercenatge < 90){
+            letterGrade = "B";
+        }
+        else if (gradePercenatge >= 70 && gradePercenatge < 80){
+            letterGrade = "C";
+        }
+        else if (gradePercenatge >= 60 && gradePercenatge < 70){
+            letterGrade = "D";
+        }
+        else if (gradePercenatge < 60){
+            letterGrade = "F";
+        }
+        else{
+            Console.WriteLine("Error");
+        }
+
+        Console.WriteLine ($"Your Letter Grade is {letterGrade}.");
+
+        if (gradePercenatge >= 70){
+            Console.WriteLine("Congrats you Passed!");
+        }
+        else{
+            Console.WriteLine("Better Luck next time.");
+        }
     }
 }
